f0fd4f20fd5e3d07b8170f3a2b250a9bd575fdc9
Yash <yashm3375@gmail.com>
2023-10-07 21:31:01 +0530

    learnt components evens and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
+import "./App.css";
 
-import './App.css';   
-
-const App = () => {
+const Person = (props) => {
   return (
-    <div className="App">
-      
+    <div>
+      <h1>name: {props.name}</h1>
+      <h2>age: {props.age}</h2>
+      <h3>favoriteColor: {props.favoriteColor}</h3>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <button onClick={() => alert("my balls! ")}>-</button>
+      <h1>0</h1>
+      <button>+</button>
+    </div>
+  );
+};
 
 export default App;
