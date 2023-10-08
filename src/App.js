7e1fe3131ca68c3c533a1fa879f32dae99af175a
Yash <yashm3375@gmail.com>
2023-10-08 13:31:24 +0530

    movieCard.jsx

--- src/App.js
+++ src/App.js
@@ -3,33 +3,40 @@ import { useEffect } from "react";
 
 import './App.css'
 import SearchIcon from './search.svg'
-//63d34323
 
 const API_URL = "https://www.omdbapi.com?apikey=63d34323";
 
-const movie1
 
 const App = () => {
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-        console.log(data.Search);
-    }
+    // const searchMovies = async (title) => {
+    //     const response = await fetch(`${API_URL}&s=${title}`);
+    //     const data = await response.json();
+        // console.log(data.Search);
+    // }
 
-    useEffect(
-        () => {
-            searchMovies('Batman');
-        }, []
-    );
+    // useEffect(
+    //     () => {
+    //         searchMovies('Batman');
+    //     }, []
+    // );
 
 
     return (
-        <div className="app">
-            <h1>Movie Land!</h1>
-            <input className="search"
-                placeholder="Search"
-            />
+      <div className="app">
+        <h1>Movie Land</h1>
+        <div className="search">
+          <input
+            placeholder="Search for Movies"
+            value="Batman"
+            onChange={() => {}}
+          />
+          <img src={SearchIcon} alt="Search" 
+          onClick={() => {}}/>
         </div>
+        <div className="container">
+          <Movie/>
+        </div>
+      </div>
     );
 }
 
