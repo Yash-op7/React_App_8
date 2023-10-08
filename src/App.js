3faf8dcf15a06439cea8e724a6831adbc972811e
Yash <yashm3375@gmail.com>
2023-10-08 13:58:07 +0530

    rendered Movies

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
-import './App.css'
-import SearchIcon from './search.svg'
+import "./App.css";
+import SearchIcon from "./search.svg";
 import Search from "./Search";
 import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com?apikey=63d34323";
 
-const movie1 = {
-  Title: "Batman v Superman: Dawn of Justice",
-  Year: "2016",
-  imdbID: "tt2975590",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-}
+// const movie1 = {
+//   Title: "Batman v Superman: Dawn of Justice",
+//   Year: "2016",
+//   imdbID: "tt2975590",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+// }
 
 const App = () => {
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-        console.log(data.Search);
-    }
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
 
-    useEffect(
-        () => {
-            searchMovies('Batman');
-        }, []
-    );
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+  const [movies, setMovies] = useState([]);
 
-    return(
-        <div className="app">
-            <h1>Movie Land</h1>
-            <Search searchIcon = {SearchIcon}/>
-            <div className="container">
-                <MovieCard movie={movie1}/>
-            </div>
+  return (
+    <div className="app">
+      <h1>Movie Land</h1>
+      <Search searchIcon={SearchIcon} />
+
+      {movies.length > 0 ? (
+        <div className="container">
+          //{" "}
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+          //{" "}
         </div>
-    );
-}
+      ) : (
+        <div>
+          <h4>No Movies fetched from the API</h4>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
