d0ca14bfddf46c85cca9bf03bb9bf1cc3d831ed4
Yash <yashm3375@gmail.com>
2023-10-08 13:35:05 +0530

    made app.js skelaton

--- src/App.js
+++ src/App.js
@@ -6,37 +6,36 @@ import SearchIcon from './search.svg'
 
 const API_URL = "https://www.omdbapi.com?apikey=63d34323";
 
+const movie1 = {
+  Title: "Batman v Superman: Dawn of Justice",
+  Year: "2016",
+  imdbID: "tt2975590",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+}
 
 const App = () => {
-    // const searchMovies = async (title) => {
-    //     const response = await fetch(`${API_URL}&s=${title}`);
-    //     const data = await response.json();
-        // console.log(data.Search);
-    // }
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
 
-    // useEffect(
-    //     () => {
-    //         searchMovies('Batman');
-    //     }, []
-    // );
+    useEffect(
+        () => {
+            searchMovies('Batman');
+        }, []
+    );
 
-
-    return (
-      <div className="app">
-        <h1>Movie Land</h1>
-        <div className="search">
-          <input
-            placeholder="Search for Movies"
-            value="Batman"
-            onChange={() => {}}
-          />
-          <img src={SearchIcon} alt="Search" 
-          onClick={() => {}}/>
+    return(
+        <div className="app">
+            <h1>Movie Land</h1>
+            <Search searchIcon = {SearchIcon}/>
+            <div className="container">
+                <MovieCard movie={movie1}/>
+            </div>
         </div>
-        <div className="container">
-          <Movie/>
-        </div>
-      </div>
     );
 }
 
