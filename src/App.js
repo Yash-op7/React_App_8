91c6abd7ba12bc4d60f7de187a7e1f8e1d46df15
Yash <yashm3375@gmail.com>
2023-10-08 10:52:30 +0530

    day2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { useEffect } from "react";
 
+import './App.css'
+import SearchIcon from './search.svg'
 //63d34323
 
 const API_URL = "https://www.omdbapi.com?apikey=63d34323";
 
+const movie1
+
 const App = () => {
     const searchMovies = async (title) => {
-        const response = async fetch(`${API_URL}&s=${title}`);
+        const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data);
+        console.log(data.Search);
     }
 
     useEffect(
         () => {
-
+            searchMovies('Batman');
         }, []
     );
 
 
     return (
-        <h1>bruh</h1>
+        <div className="app">
+            <h1>Movie Land!</h1>
+            <input className="search"
+                placeholder="Search"
+            />
+        </div>
     );
 }
 
